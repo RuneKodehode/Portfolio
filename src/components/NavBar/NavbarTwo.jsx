1903ebab425012ce41d9582bfb6ba16a0b50f03d
RuneKodehode <rune.kodehode@gmail.com>
2023-03-13 14:37:31 +0100

    fixed navbartwo

--- src/components/NavBar/NavbarTwo.jsx
+++ src/components/NavBar/NavbarTwo.jsx
@@ -2,31 +2,21 @@ import { AiFillGithub } from "react-icons/ai";
 import { Link } from "react-router-dom";
 function NavBarTwo() {
   return (
-    <div className="grid row-start-1 col-start-3 col-end-2  sm:col-end-3 w-auto">
-      <div className="flex flex-row self-start justify-self-end text-white sm:flex-col">
+    <div className="grid row-start-1 col-start-3 col-end-2  sm:col-end-3 w-full">
+      <div className="flex self-start justify-self-end text-white flex-col -mr-6 sm:mr-0">
         <a
           href="https://github.com/RuneKodehode?tab=repositories"
           target="_blank"
           rel="noreferrer noopener"
-          className="w-10 h-10 sm:w-32 sm:h-16"
+          className="grid place-content-center rounded-lg text-black text-center active:scale-90 text-xs bg-white hover:border-orange-600 hover:border-2 font-bold  hover:border-orange-600 hover:border-2 sm:w-32  sm:h-12 sm:text-lg m-1"
         >
-          {" "}
-          <button
-            id="githubBtn"
-            aria-label="Button to Rune's Github repository"
-            className="w-10 h-10 sm:w-32 sm:h-16 bg-black rounded-lg border hover:bg-zinc-800 active:animate-ping"
-          >
-            <AiFillGithub className="w-full h-full" />
-          </button>
+          <AiFillGithub className="h-10 w-16" />
         </a>
-        <Link to="/Contact" className="border-solid w-10 h-10 sm:w-32 sm:h-16">
-          <button
-            id="contactBtn"
-            aria-label="Contact information button"
-            className="border rounded-lg text-white text-center  text-xs w-16 h-10 sm:w-32 sm:h-16 sm:text-xl bg-black hover:bg-zinc-800 active:animate-ping"
-          >
-            contact
-          </button>
+        <Link
+          to="/Contact"
+          className="grid place-content-center rounded-lg h-10 w-16 text-black active:scale-90 text-center  text-xs bg-white hover:border-orange-600 hover:border-2 font-bold  hover:border-orange-600 hover:border-2 sm:w-32  sm:h-12 sm:text-lg m-1"
+        >
+          contact
         </Link>
       </div>
     </div>
