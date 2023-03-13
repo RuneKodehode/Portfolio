250a9c0818e64a6a7cb106ad201fc231f82ff78a
RuneKodehode <rune.kodehode@gmail.com>
2023-03-13 13:38:47 +0100

    playing around with color and background choices

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,23 +8,18 @@ function NavBar() {
 
   return (
     <div className="grid row-start-3 col-start-1 sm:row-start-1 sm:col-start-1">
-      <div className="flex flex-row sm:flex-col" id="navbar">
-        {/* here we have a div with a <link> that navigates to the default homepage of the site. within the button you see an svg that represents a house on the button itself.  */}
-
+      <div className="flex sm:flex-col" id="navbar">
         <Link
           to="/"
-          className="w-16 h-12 sm:w-32 text-black border-none border bg-orange-400 rounded-lg flex hover:bg-orange-300 active:animate-ping"
+          className="w-16 h-12 sm:w-32 text-black font-bold  bg-white  rounded-lg flex hover:invert active:animate-ping m-1"
         >
-          <button
-            aria-label="Home button"
-            className="border rounded-lg w-full h-full"
-          >
+          <button aria-label="Home button" className="rounded-lg w-full h-full">
             <BiHomeAlt2 className="w-10 h-12 sm:w-32  mr-auto ml-auto" />
           </button>
         </Link>
         <button
           to="/menu"
-          className="border w-16 h-12 rounded-lg text-white text-center  text-xs bg-orange-500 sm:w-32  sm:h-12 sm:text-lg hover:bg-orange-400 active:animate-ping"
+          className="w-16 h-12 rounded-lg font-bold text-black text-center  text-xs bg-white sm:w-32  sm:h-12 sm:text-lg hover:invert active:animate-ping m-1"
           aria-label="open or close the navigation menu"
           onClick={() => {
             setShowNav(!showNav);
@@ -33,12 +28,12 @@ function NavBar() {
         >
           projects
         </button>
-        <div className="flex flex-col w-16">
+        <div className="flex flex-col">
           {showNav && (
-            <Link to="/ProjectOne" className="w-32">
+            <Link to="/ProjectOne">
               <button
                 aria-label="Parallax text button"
-                className="border w-16 h-12 rounded-lg text-white text-center  text-xs bg-orange-600 sm:w-32  sm:h-12 sm:text-lg hover:bg-orange-500 active:animate-ping"
+                className="w-16 h-12 rounded-lg text-black text-center font-bold  text-xs bg-white  sm:w-32  sm:h-12 sm:text-lg hover:invert active:animate-ping m-1"
                 onClick={() => {
                   setHideClose((prevHideClose) =>
                     prevHideClose !== 1 ? 1 : 0
@@ -50,12 +45,9 @@ function NavBar() {
             </Link>
           )}
           {showNav && hideClose === 1 && (
-            <div
-              aria-label="Show Parallax website"
-              className="flex flex-col w-16"
-            >
+            <div aria-label="Show Parallax website" className="flex flex-col">
               <Link to="/AboutProjOne" className="w-auto h-auto">
-                <button className="border border-blue-300 w-16 h-10 rounded-lg text-white text-center  text-xs bg-orange-900 hover:bg-orange-800 sm:w-32  sm:h-12 sm:text-lg">
+                <button className=" w-16 h-10 rounded-lg text-black text-center  text-xs bg-slate-400 font-bold  hover:invert sm:w-32  sm:h-12 sm:text-lg m-1">
                   info
                 </button>
               </Link>
@@ -67,7 +59,7 @@ function NavBar() {
               >
                 <button
                   aria-label="Button to Parallax Github"
-                  className="border border-blue-300 w-16 h-10 rounded-lg text-white text-center  text-xs bg-orange-900 hover:bg-orange-800 sm:w-32  sm:h-12 sm:text-lg"
+                  className=" w-16 h-10 rounded-lg text-black text-center  text-xs bg-slate-400 font-bold  hover:invert sm:w-32  sm:h-12 sm:text-lg m-1"
                 >
                   Github
                 </button>
@@ -80,7 +72,7 @@ function NavBar() {
             <Link to="/ProjectTwo" className="">
               <button
                 aria-label="Pokedex text button"
-                className="border w-16 h-12 rounded-lg text-white text-center  text-xs bg-orange-600 sm:w-32  sm:h-12 sm:text-lg hover:bg-orange-500 active:animate-ping"
+                className="w-16 h-12 rounded-lg text-black text-center font-bold  text-xs bg-white  sm:w-32  sm:h-12 sm:text-lg hover:invert active:animate-ping m-1"
                 id="pokedexBtn"
                 onClick={() => {
                   setHideClose((prevHideClose) =>
@@ -93,11 +85,11 @@ function NavBar() {
             </Link>
           )}
           {showNav && hideClose === 2 && (
-            <div className="flex flex-col ">
+            <div className="flex flex-col">
               <Link to="/AboutProjTwo" className="w-auto h-auto ">
                 <button
                   aria-label="show Pokedex"
-                  className="border border-blue-300 w-16 h-10 rounded-lg text-white text-center  text-xs bg-orange-800 hover:bg-orange-700 sm:w-32  sm:h-12 sm:text-lg"
+                  className=" w-16 h-10 rounded-lg text-black text-center  text-xs bg-slate-400 font-bold  hover:invert sm:w-32  sm:h-12 sm:text-lg m-1"
                 >
                   info
                 </button>
@@ -110,7 +102,7 @@ function NavBar() {
               >
                 <button
                   aria-label="Link to Pokedex Github"
-                  className="border border-blue-300 w-16 h-10 rounded-lg text-white text-center  text-xs bg-orange-800 hover:bg-orange-700 sm:w-32  sm:h-12 sm:text-lg"
+                  className=" w-16 h-10 rounded-lg text-black text-center  text-xs bg-slate-400 font-bold  hover:invert sm:w-32  sm:h-12 sm:text-lg m-1"
                 >
                   Github
                 </button>
@@ -118,13 +110,13 @@ function NavBar() {
             </div>
           )}
         </div>
-        <div className="flex flex-col h-8 w-20">
+        <div className="flex flex-col">
           {showNav && (
             <Link to="/Breakout" className=" w-aut h-auto">
               <button
                 aria-label="Button to show Breakout text"
                 id="breakout"
-                className="border w-16 h-12 rounded-lg text-white text-center  text-xs bg-orange-600 sm:w-32  sm:h-12 sm:text-lg hover:bg-orange-500 active:animate-ping"
+                className="w-16 h-12 rounded-lg text-black text-center font-bold  text-xs bg-white  sm:w-32  sm:h-12 sm:text-lg hover:invert active:animate-ping m-1"
                 onClick={() => {
                   setHideClose((prevHideClose) =>
                     prevHideClose !== 3 ? 3 : 0
@@ -140,7 +132,7 @@ function NavBar() {
               <Link to="/AboutBreakout" className="w-auto h-auto ">
                 <button
                   aria-label="Button to Play Breakout"
-                  className="border border-blue-300 w-16 h-10 rounded-lg text-white text-center  text-xs bg-orange-800 hover:bg-orange-700 sm:w-32  sm:h-12 sm:text-lg"
+                  className=" w-16 h-10 rounded-lg text-black text-center  text-xs bg-slate-400 font-bold  hover:invert sm:w-32  sm:h-12 sm:text-lg m-1"
                 >
                   info
                 </button>
@@ -152,7 +144,7 @@ function NavBar() {
               >
                 <button
                   aria-label="Button to Breakout Github link"
-                  className="border border-blue-300 w-16 h-10 rounded-lg text-white text-center  text-xs bg-orange-800 hover:bg-orange-700 sm:w-32  sm:h-12 sm:text-lg"
+                  className=" w-16 h-10 rounded-lg text-black text-center  text-xs bg-slate-400 font-bold  hover:invert sm:w-32  sm:h-12 sm:text-lg m-1"
                 >
                   Github
                 </button>
